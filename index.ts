79ade93e78b16b0b89b65dd38b47b8d778d2b6ff
Oriun <emmanuel.nuiro@epitech.eu>
2023-11-30 15:14:57 +0100

    add flush feature

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-export type Throttle<T extends unknown[], S> = (...args: T) => Promise<S>;
+export type Throttle<T extends unknown[], S> = {
+    (...args: T): Promise<S>;
+    flush(): void;
+}
 export type Resolve<S> = (value: S | PromiseLike<S>) => void;
 
 export type ThrottleCallback = <T extends unknown[], S>(args: T[], promises: Resolve<S>[]) => void;
@@ -7,13 +10,14 @@ function createThrottle<T extends unknown[], S>(callback: ThrottleCallback, dela
     const ArgumentStack: T[] = [];
     const PromiseStack: Resolve<S>[] = [];
     let promise: Promise<any> | null = null;
-    return function throttle(...args: T) {
+    let timeout: NodeJS.Timeout | null = null;
+    function throttle(...args: T) {
       return new Promise<S>((resolve) => {
         ArgumentStack.push(args);
         PromiseStack.push(resolve);
         if (!promise) {
           promise = new Promise<void>((resolve2) => {
-            setTimeout(() => {
+            timeout = setTimeout(() => {
               callback(
                 ArgumentStack.splice(0, ArgumentStack.length),
                 PromiseStack.splice(0, PromiseStack.length)
@@ -27,6 +31,13 @@ function createThrottle<T extends unknown[], S>(callback: ThrottleCallback, dela
         return promise;
       });
     };
+    throttle.flush = () => {
+        clearTimeout(timeout!);
+        promise = null;
+        ArgumentStack.splice(0, ArgumentStack.length);
+        PromiseStack.splice(0, PromiseStack.length);
+    }
+    return throttle;
   };
   
   export default createThrottle;
